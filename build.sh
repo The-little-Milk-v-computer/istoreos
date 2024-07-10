abf07de9a582c57156ddd9df2e7722051a17aee1
draftbottle <2641473760@qq.com>
2024-07-10 13:41:07 +0800

    添加一键编译脚本

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
-echo "src-link  vizos  ../feeds-vizos" >> feeds.conf.default
+# echo "src-link  vizos  ../feeds-vizos" >> feeds.conf.default
 cat feeds.conf.default
+echo -e "\033[32mInstalling the packages now...\033[0m"
 ./scripts/feeds update  -a
 ./scripts/feeds install -a
 ./scripts/feeds install -f sophgo
 ./scripts/feeds install -f uboot-sophgo
 
-echo '请选择开发板型号:'
+echo -e '\033[32mSelect a target to build:\033[0m'
 echo '1.milkv-duo'
 echo '2.milkv-duo256m'
 echo '3.milkv-duos'
@@ -14,20 +15,32 @@ read board
 
 case "$board" in
 "1")
-    cp ../feeds-vizos/cofigs/cv180x-config .config
+    cp ./feeds/vizos/cofigs/cv180x-config .config
 ;;
 "2")
-    cp ../feeds-vizos/configs/cv181x-milkv-duo256m-config .config
+    cp ./feeds/vizos/configs/cv181x-milkv-duo256m-config .config
 ;;
 "3")
-    cp ../feeds-vizos/configs/cv181x-milkv-duos-config .config
+    cp ./feeds/vizos/configs/cv181x-milkv-duos-config .config
 ;;
 "4")
-    cp ../feeds-vizos/configs/cv180x-config .config
+    cp ./feeds/vizos/configs/cv181x-config .config
 ;;
 esac
 
-echo -e "$(nproc) thread compile"
+if [ -d "/opt/host-tools/gcc/riscv64-linux-musl-x86_64" ]; then
+    echo 'the host-tools exits.'
+elif [ -d "./host-tools/gcc/riscv64-linux-musl-x86_64" ]; then
+    sed -i 's|/opt/host-tools/gcc/riscv64-linux-musl-x86_64|./host-tools/gcc/riscv64-linux-musl-x86_64|g' .config
+    echo 'the host-tools exits.'
+else
+    echo -e '\033[32mthe host-tools not exits, downloading it now...\033[0m'
+    sed -i 's|/opt/host-tools/gcc/riscv64-linux-musl-x86_64|./host-tools/gcc/riscv64-linux-musl-x86_64|g' .config
+    git clone https://github.com/milkv-duo/host-tools.git
+fi
+
+echo -e "\033[32m$(nproc) thread compile.\033[0m"
+make download -j$(nproc)
 make -j$(nproc) || make package/feeds/vizos/uboot-sophgo/compile V=s
-make -j8 V=s
-echo "::set-output name=status::success"
+make -j$(nproc) V=s
+echo -e "\033[32mThe $board milkv-duo board build successful.\033[0m"
